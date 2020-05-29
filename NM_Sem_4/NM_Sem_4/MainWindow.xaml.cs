93907c9713ebc9a3142dc57ca77fe02bd463a5f5
Ivan Filimonov <thesuppafila@gmail.com>
2020-05-29 11:28:36 +0300

    some changes UI

--- NM_Sem_4/NM_Sem_4/MainWindow.xaml.cs
+++ NM_Sem_4/NM_Sem_4/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NM_Sem_4
 {
@@ -70,11 +60,17 @@ namespace NM_Sem_4
             switch (comboBox.SelectedIndex)
             {
                 case 0:
-                    func = new FuncDelegate(f23); break;
+                    func = new FuncDelegate(f23);
+                    EquationLabel.Content = "y' = sin(y - 2x) + 2y";
+                    break;
                 case 1:
-                    func = new FuncDelegate(f25); break;
+                    func = new FuncDelegate(f25);
+                    EquationLabel.Content = "y' = 2xy + 5x - y + y^2";
+                    break;
                 case 2:
-                    func = new FuncDelegate(test); break;
+                    func = new FuncDelegate(test);
+                    EquationLabel.Content = "y' = x^2 * y^(1/3)";
+                    break;
             }
         }
 
@@ -86,15 +82,15 @@ namespace NM_Sem_4
             FillDataGrid(mainDataGrid);
         }
 
-        public double f25(double x, double y) => 2 * x * y + 5 * x - y + Math.Pow(y, 2);
-
         public double f23(double x, double y) => Math.Sin(y - 2 * x) + 2 * y;
 
+        public double f25(double x, double y) => 2 * x * y + 5 * x - y + Math.Pow(y, 2);
+        
         public double test(double x, double y) => x * x * Math.Pow(y, 1 / 3);
 
         public void Eiler(double x, double y) //эйлер
         {
-            int n = (int)Math.Abs((B - A) / Step);
+            int n = (int)Math.Abs((B - A) / Step)+1;
             xList = new List<double>() { x };
             yListE = new List<double>() { y };
             for (int i = 1; i < n; i++)
@@ -104,12 +100,11 @@ namespace NM_Sem_4
                 yListE.Add(y);
                 xList.Add(x);
             }
-            //FillDataGrid(EilerDataGrid);
         }
 
         public void Adams(double x, double y) //адамс
         {
-            int n = (int)Math.Abs((B - A) / Step);
+            int n = (int)Math.Abs((B - A) / Step)+1;
             xList = new List<double>() { x };
             yListA = new List<double>() { y };
             Q = new List<double>() { Step * func(x, y) };
@@ -136,7 +131,6 @@ namespace NM_Sem_4
                 Q.Add(Step * func(x, y));
             }
 
-            //FillDataGrid(AdamsDataGrid);
         }
 
         public double DeltaY(int n)
@@ -149,7 +143,7 @@ namespace NM_Sem_4
 
         public void RK(double x, double y) //рунге-кутт
         {
-            int n = (int)Math.Abs((B - A) / Step);
+            int n = (int)Math.Abs((B - A) / Step)+1;
             xList = new List<double>() { x };
             yListRK = new List<double>() { y };
 
@@ -165,7 +159,6 @@ namespace NM_Sem_4
                 yListRK.Add(y);
                 xList.Add(x);
             }
-            //FillDataGrid(RKDataGrid);
         }
 
         public void FillDataGrid(DataGrid dg)
@@ -176,11 +169,9 @@ namespace NM_Sem_4
             dt.Columns.Add("Адамс");
             dt.Columns.Add("Рунге-Кутта");
 
-
-
             for (int i = 0; i < xList.Count; i++)
             {
-                dt.Rows.Add(xList[i], yListE[i], yListA[i], yListRK[i]);
+                dt.Rows.Add(Math.Round(xList[i],4), Math.Round(yListE[i],4), Math.Round(yListA[i],4), Math.Round(yListRK[i],4));
             }
             dg.ItemsSource = dt.DefaultView;
             if (EilerCheckBox.IsChecked == false)
@@ -189,7 +180,6 @@ namespace NM_Sem_4
                 mainDataGrid.Columns[2].Visibility = Visibility.Hidden;
             if (RKCheckBox.IsChecked == false)
                 mainDataGrid.Columns[3].Visibility = Visibility.Hidden;
-
         }
     }
 }
